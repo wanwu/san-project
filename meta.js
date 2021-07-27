8e07628b04ea6a0598281fb86a4a5cf8dffdc9c6
liuhuyue <liuhuyue@baidu.com>
2021-07-27 17:02:15 +0800

    refactor: 删去对 inquirer 的兼容

--- meta.js
+++ meta.js
@@ -56,14 +56,12 @@ Start with ${chalk.bold('yarn/npm start')}`,
             message: '选择模板引擎',
             choices: [
                 {
-                    title: 'Smarty（百度内部）',    // 兼容 prompts
-                    name: 'Smarty（百度内部）',
+                    title: 'Smarty（百度内部）',
                     value: 'smarty',
                     short: 'Smarty'
                 },
                 {
-                    title: '纯 HTML',   // 兼容 prompts
-                    name: '纯 HTML',
+                    title: '纯 HTML',
                     value: 'html',
                     short: 'HTML'
                 }
@@ -79,20 +77,17 @@ Start with ${chalk.bold('yarn/npm start')}`,
             message: '选择 ESLint 配置',
             choices: [
                 {
-                    title: '@ecomfe/eslint-config (https://github.com/ecomfe/eslint-config)',   // 兼容 prompts
-                    name: '@ecomfe/eslint-config (https://github.com/ecomfe/eslint-config)',
+                    title: '@ecomfe/eslint-config (https://github.com/ecomfe/eslint-config)',
                     value: 'ecomfe',
                     short: 'ecomfe'
                 },
                 {
-                    title: 'Standard (https://github.com/standard/standard)',   // 兼容 prompts
-                    name: 'Standard (https://github.com/standard/standard)',
+                    title: 'Standard (https://github.com/standard/standard)',
                     value: 'standard',
                     short: 'Standard'
                 },
                 {
-                    title: 'Airbnb (https://github.com/airbnb/javascript)', // 兼容 prompts
-                    name: 'Airbnb (https://github.com/airbnb/javascript)',
+                    title: 'Airbnb (https://github.com/airbnb/javascript)',
                     value: 'airbnb',
                     short: 'Airbnb'
                 }
@@ -113,14 +108,12 @@ Start with ${chalk.bold('yarn/npm start')}`,
             message: '选择示例代码类型：',
             choices: [
                 {
-                    title: 'san-store (推荐)',  // 兼容 prompts
-                    name: 'san-store (推荐)',
+                    title: 'san-store (推荐)',
                     value: 'store',
                     short: 'san-store'
                 },
                 {
-                    title: 'normal',    // 兼容 prompts
-                    name: 'normal',
+                    title: 'normal',
                     value: 'normal',
                     short: 'normal'
                 }
@@ -131,20 +124,17 @@ Start with ${chalk.bold('yarn/npm start')}`,
             message: '选择 CSS 预处理器',
             choices: [
                 {
-                    title: 'Less（推荐）',  // 兼容 prompts
-                    name: 'Less（推荐）',
+                    title: 'Less（推荐）',
                     value: 'less',
                     short: 'Less'
                 },
                 {
-                    title: 'Sass',  // 兼容 prompts
-                    name: 'Sass',
+                    title: 'Sass',
                     value: 'sass',
                     short: 'Sass'
                 },
                 {
-                    title: 'Stylus',    // 兼容 prompts
-                    name: 'Stylus',
+                    title: 'Stylus',
                     value: 'stylus',
                     short: 'Stylus'
                 }
